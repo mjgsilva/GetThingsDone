f28721acb8a9202ba543fa8840b14506539f1b91
Mário Silva <mjgs.biz@gmail.com>
2015-02-06 18:37:36 +0000

    Project Skeleton

---
+++ GetThingsDone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetThingsDone
+//
+//  Created by Mário Silva on 06/02/15.
+//  Copyright (c) 2015 Mário Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
