c83271fce9a2bf815a736059b15287bd93243e8e
Mário Silva <mjgs.biz@gmail.com>
2015-03-28 23:54:14 +0000

    CI: ListDetailVC

---
+++ GetThingsDone/ListDetailViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ListDetailViewController.swift
+//  GetThingsDone
+//
+//  Created by Mário Silva on 28/03/15.
+//  Copyright (c) 2015 Mário Silva. All rights reserved.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+    func listDetailViewControllerDidCancel(controller: ListDetailViewController)
+    func listDetailViewController(controller: ListDetailViewController,
+        didFinishAddingChecklist checklist: Checklist)
+    func listDetailViewController(controller: ListDetailViewController,
+            didFinishEditingChecklist checklist: Checklist)
+}
+
+class ListDetailViewController: UITableViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 44
+        
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.enabled = true
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+            checklist.name = textField.text
+            delegate?.listDetailViewController(self, didFinishEditingChecklist: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text)
+            delegate?.listDetailViewController(self, didFinishAddingChecklist: checklist)
+        }
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText: NSString = textField.text
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        doneBarButton.enabled = (newText.length > 0)
+        return true
+    }
+
+}
