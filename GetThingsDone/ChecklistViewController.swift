d556318a94f2d45beef0d20e54988a08f70fb2e7
Mário Silva <mjgs.biz@gmail.com>
2015-04-06 23:59:41 +0100

    CI: UI Enhancements

--- GetThingsDone/ChecklistViewController.swift
+++ GetThingsDone/ChecklistViewController.swift
@@ -65,6 +65,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     func configureCheckmarkForCell(cell: UITableViewCell, withChecklistItem item: ChecklistItem) {
         
         let label = cell.viewWithTag(1001) as UILabel
+        label.tintColor = view.tintColor
         
         if item.checked {
             label.text = "✓"
